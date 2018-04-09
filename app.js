a5a57f61fb2c8c83d6f6bbb7b1327e5586760cce
maksimn <maksim1989sovunion@yandex.ru>
2018-04-09 10:02:34 +0300

    first request

---
+++ app.js
@@ -0,0 +1,28 @@
+﻿const MongoClient = require('mongodb').MongoClient;
+ 
+const url = 'mongodb://localhost:27017'; // URL соединения
+const dbName = 'CarAccidents'; // Имя базы данных
+
+function selectCoordinatesOfAllCarAccidents(db, callback) {
+    db.collection('CarAccidents').find({})
+                                 .project( { "geo_code": 1 })
+								                 .toArray((err, docs) => {
+        if (err) {
+            throw new Error('Error querying data.');
+        }
+        
+        console.log('Found the following documents: ', docs);
+        
+        callback();
+    });
+}
+
+MongoClient.connect(url, (err, client) => {
+    if (err) {
+      throw new Error('Not connected correctly to server');
+    }
+    
+    const db = client.db(dbName);
+    
+    selectCoordinatesOfAllCarAccidents(db, () => { client.close(); });
+});
