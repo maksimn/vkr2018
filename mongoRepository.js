f9739f23a2b777514b2ea62d1980b78ffb1efa6f
maksimn <maksim1989sovunion@yandex.ru>
2018-04-10 10:31:45 +0300

    refactor shared code from mongoRepository properties

--- mongoRepository.js
+++ mongoRepository.js
@@ -2,50 +2,44 @@ const MongoClient = require('mongodb').MongoClient;
 
 const config = require('./config.json');
 
-const mongoRepository = {
-    findAccidentsIdsAndGeoCoordinates: () => {
-        return new Promise((resolve, reject) => {
-            MongoClient.connect(config.DB_URL).then(client => {
-                const db = client.db(config.DB_NAME);
-                
-                db.collection('CarAccidents')
-                    .find()
-                    .project({ "geo_code": 1 })
-                    .toArray()
-                    .then(docs => {
-                        resolve(docs);
-                        client.close();
-                    }).catch(err => {
-                        reject(err);
-                    });
-            }).catch(err => {
-                reject(err);
-            });
+const connectAndQueryDB = query => (
+    new Promise((resolve, reject) => {
+        MongoClient.connect(config.DB_URL).then(client => {
+            const db = client.db(config.DB_NAME);
+            
+            query(db).toArray()
+                .then(docs => {
+                    resolve(docs);
+                    client.close();
+                }).catch(err => {
+                    reject(err);
+                });
+        }).catch(err => {
+            reject(err);
         });
-    },
-    
-    findCarAccidentsByCoordinates: (latitude, longitude) => {
-        return new Promise((resolve, reject) => {
-            MongoClient.connect(config.DB_URL).then(client => {
-                const db = client.db(config.DB_NAME);
+    })
+);
 
-                db.collection('CarAccidents')
-                    .find({
-                        'geo_code.latitude': latitude,
-                        'geo_code.longitude': longitude
-                    })
-                    .toArray()
-                    .then(docs => {
-                        resolve(docs);
-                        client.close();
-                    }).catch(err => {
-                        reject(err);
-                    });
-            }).catch(err => {
-                reject(err);
-            });
-        });
-    }
+const mongoRepository = {
+    findAccidentsIdsAndGeoCoordinates: () => (
+        connectAndQueryDB(db => (
+            db.collection('CarAccidents')
+                .find()
+                .project({ "geo_code": 1 })
+            )
+        )
+    ),
+    
+    findCarAccidentsByCoordinates: (latitude, longitude) => (
+        connectAndQueryDB(db => (
+            db.collection('CarAccidents')
+                .find({
+                    'geo_code.latitude': latitude,
+                    'geo_code.longitude': longitude
+                })
+            )
+        )
+    )
 };
 
 module.exports = mongoRepository;
