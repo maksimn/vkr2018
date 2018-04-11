608361f134aa2ec3f03ead91556f77be32216b04
maksimn <maksim1989sovunion@yandex.ru>
2018-04-11 15:30:47 +0300

    running async calls sequentially

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 ﻿const mongoRepository = require('./mongoRepository');
 
-mongoRepository.findAccidentsIdsAndGeoCoordinates()
-    .then(docs => {
-        console.log(
-            `${docs.length} documents found.\n\n`,
-            'First document: \n',
-            docs[0]
-        );
-    }).catch(err => {
-        console.log(err);
+new Promise((resolve, reject) => {
+    mongoRepository.findAccidentsIdsAndGeoCoordinates()
+        .then(docs => {
+            console.log(
+                `${docs.length} documents found.\n\n`,
+                'First document: \n',
+                docs[0]
+            );
+            resolve();
+        }).catch(err => {
+            reject(err);
+        });
+}).then(() => {
+    return new Promise(function (resolve, reject) {
+        mongoRepository.findCarAccidentsByCoordinates(55.4224, 37.2333)
+            .then(docs => {
+                console.log(
+                    `findCarAccidentsByCoordinates(): ${docs.length} documents found.\n\n`,
+                    docs, '\n\n'
+                );
+                resolve();
+            }).catch(err => {
+                reject(err);
+            });
     });
-
-mongoRepository.findCarAccidentsByCoordinates(55.4224, 37.2333)
-    .then(docs => {
-        console.log(
-            `findCarAccidentsByCoordinates(): ${docs.length} documents found.\n\n`,
-            docs, '\n\n'
-        );
-    }).catch(err => {
-        console.log(err);
-    });
+}, err => {
+    console.log(err);
+});
