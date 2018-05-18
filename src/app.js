48228d9fd2c8487c5cb4b137fe1caca1dc462537
maksimn <maksim1989sovunion@yandex.ru>
2018-05-18 10:14:32 +0300

    main page, add bootstrap

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const config = require('./config.json');
 
 const app = express();
 
+app.use(express.static(__dirname + '/..'));
+
 app.get('/', (req, res) => {
     res.sendFile('./index.html',  { root: __dirname });
 });
