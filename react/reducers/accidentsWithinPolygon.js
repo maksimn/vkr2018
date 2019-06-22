49d2c6ed6ddbb5a97095e9cd2c6b585ec4c1e0d8
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-06-22 09:10:49 +0300

    [refactor] move groupAccidentsByLocation to a separate file

--- react/reducers/accidentsWithinPolygon.js
+++ react/reducers/accidentsWithinPolygon.js
@@ -3,38 +3,7 @@ import {
     FIND_ACCIDENTS_WITHIN_POLYGON_SUCCESS,
     FIND_ACCIDENTS_WITHIN_POLYGON_ERROR
 } from '../actions/constants';
-
-const groupAccidentsByLocation = dataArray => {
-    if (dataArray.length) {
-        let current = dataArray[0];
-        const groupResultArray = [{
-            location: current.location,
-            accidentIds: [current._id]
-        }];
-        let groupInd = 0;
-        
-        for (let i = 1; i < dataArray.length; i++) {
-            const current = dataArray[i];
-            const {coordinates} = current.location;
-            const groupResultElementCoords = groupResultArray[groupInd].location.coordinates;
-
-            if (coordinates[0] === groupResultElementCoords[0] &&
-                coordinates[1] === groupResultElementCoords[1]) {
-                groupResultArray[groupInd].accidentIds.push(current._id);
-            } else {
-                groupResultArray.push({
-                    location: current.location,
-                    accidentIds: [current._id]
-                });
-                groupInd++;
-            }
-        }
-
-        return groupResultArray;
-    } else {
-        return [];
-    }
-};
+import {groupAccidentsByLocation} from '../groupAccidentsByLocation';
 
 const accidentsWithinPolygon = (state = null, action) => {
     switch (action.type) {
